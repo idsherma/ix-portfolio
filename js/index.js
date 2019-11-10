2015a21587ccc2fd12490e637af7d168db966d6b
Ivory Sherman <ivorysherman@Ivorys-MacBook-Pro.local>
2019-11-10 18:21:00 -0500

    final build

--- js/index.js
+++ js/index.js
@@ -2,6 +2,24 @@
     scs(window.jQuery, window, document);
     }(function($, window, document) {
 
-        //console.log('hi world');
+        let checkbox = document.querySelector('input[name=theme]');
+
+        checkbox.addEventListener('change' ,function(){
+         if(this.checked){
+             trans();
+             document.documentElement.setAttribute('data-theme', 'dark');
+         }
+         else{
+             trans();
+             document.documentElement.setAttribute('data-theme', 'light');
+         }
+        });
+        
+        let trans = () => {
+                 document.documentElement.classList.add('transition');
+                 window.setTimeout(() => {
+                     document.documentElement.classList.remove('transition')
+                 }, 1000);
+             }
     })
 );
